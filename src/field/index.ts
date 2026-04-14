8d90d434547d6b9bd215e06d0690d8fa26b232fd
cumul <gg6123@naver.com>
2026-04-14 22:47:15 +0900

    export `<Controller>` as its original name

--- src/field/index.ts
+++ src/field/index.ts
@@ -1,4 +1,4 @@
-export { Controller as FieldController } from './components.js';
+export { Controller } from './components.js';
 export { field } from './helpers.js';
 export { useField, useFieldInput, type UseFieldResult } from './hooks.js';
 export type { Field } from './types.js';
