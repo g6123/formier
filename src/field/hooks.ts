69f493cc627076ff5ada54f4644820cd8f2e3e36
cumul <gg6123@naver.com>
2026-04-15 23:01:51 +0900

    UseFieldResult.{handleChange => setValue}

--- src/field/hooks.ts
+++ src/field/hooks.ts
@@ -1,6 +1,7 @@
 import React, { useCallback } from 'react';
 import { useMergeRefs } from 'react-merge-refs';
 import { useAtomValue } from '../internal/atom.js';
+import { isFunction } from '../internal/misc.js';
 import { ValidationResult } from '../validation/result.js';
 import { Field } from './types.js';
 
@@ -8,7 +9,7 @@ export interface UseFieldResult<Input, Value> {
   ref: React.Ref<any>;
   value: Input;
   state: ValidationResult<Value>;
-  handleChange: (value: Input) => void;
+  setValue: (value: React.SetStateAction<Input>) => void;
 }
 
 export function useField<Input, Value>(field: Field<Input, Value>, ref?: React.Ref<any>): UseFieldResult<Input, Value> {
@@ -16,14 +17,18 @@ export function useField<Input, Value>(field: Field<Input, Value>, ref?: React.R
   const value = useAtomValue(field.input);
   const state = useAtomValue(field.state);
 
-  const handleChange = useCallback(
-    (nextValue: Input) => {
-      field.set(nextValue);
+  const setValue = useCallback(
+    (action: React.SetStateAction<Input>) => {
+      if (isFunction(action)) {
+        field.set(action(field.input.get()));
+      } else {
+        field.set(action);
+      }
     },
     [field],
   );
 
-  return { ref: mergedRef, value, state, handleChange };
+  return { ref: mergedRef, value, state, setValue };
 }
 
 export function useFieldInput<InputT, ValueT>(store: Field<InputT, ValueT>): InputT {
