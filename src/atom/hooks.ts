fee289b57fa0be43e77e3226386cfa09733e5621
cumul <gg6123@naver.com>
2026-04-17 01:15:14 +0900

    add useSetAtom(), useAtom()

--- src/atom/hooks.ts
+++ src/atom/hooks.ts
@@ -1,6 +1,25 @@
+import React, { useCallback } from 'react';
 import { useSyncExternalStore } from 'use-sync-external-store';
-import { ReadonlyAtom } from './types.js';
+import { isFunction } from '../internal/misc.js';
+import { Atom, ReadonlyAtom } from './types.js';
 
 export function useAtomValue<T>(atom: ReadonlyAtom<T>): T {
   return useSyncExternalStore(atom.subscribe, atom.get);
+}
+
+export function useSetAtom<T>(atom: Atom<T>): React.Dispatch<React.SetStateAction<T>> {
+  return useCallback(
+    (action: React.SetStateAction<T>) => {
+      if (isFunction(action)) {
+        atom.set(action(atom.get()));
+      } else {
+        atom.set(action);
+      }
+    },
+    [atom],
+  );
+}
+
+export function useAtom<T>(atom: Atom<T>) {
+  return [useAtomValue(atom), useSetAtom(atom)] as const;
 }
--- src/atom/index.ts
+++ src/atom/index.ts
@@ -1,4 +1,4 @@
 export { Read, type ReadProps } from './components.js';
-export { useAtomValue } from './hooks.js';
+export { useAtom, useAtomValue, useSetAtom } from './hooks.js';
 export { atom } from './store.js';
 export type { Atom, ReadonlyAtom } from './types.js';
